33b612b9c833e73f6c1ad0066a5befb8ab022f0a
Scott Atkins <satkins16@gmail.com>
2017-11-11 16:05:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,3 +35,7 @@ BoardMember.prototype.veto = function() {
 BoardMember.prototype.approve = function(){
      return 'You can do that!'
    }
+
+BoardMember.prototype.doCharity = function() {
+  return 'I like to help people.'
+}
